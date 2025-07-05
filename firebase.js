4b4972c0aaa785d4c1f1dbccab1499a2c3852b86
shudhanshunidhi <sdhanshu504@gmail.com>
2025-07-05 10:01:32 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 
@@ -6,11 +5,12 @@ const firebaseConfig = {
   apiKey:"AIzaSyATczaHTtHh_84Lt9kVykHWmSJzxHAY3Dw",
   authDomain: "gyanpeeth-77039.firebaseapp.com",
   projectId: "gyanpeeth-77039",
-  appId: "1:584911737233:web:4c9fd8a65bf9a39dc70a30",
-  storageBucket: "gyanpeeth-77039.firebasestorage.app",
+  storageBucket: "gyanpeeth-77039.appspot.com",    // <-- corrected here
   messagingSenderId: "584911737233",
+  appId: "1:584911737233:web:4c9fd8a65bf9a39dc70a30",
   measurementId: "G-B7HCHPFNGZ"
 };
+
 const app = initializeApp(firebaseConfig);
-//const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);   // keep commented if you don't use analytics
 export const auth = getAuth(app);

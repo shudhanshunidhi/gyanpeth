d9dff73fccb5efb005a1d64a32ae9e0916caa416
shudhanshunidhi <sdhanshu504@gmail.com>
2025-07-05 09:50:06 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -12,4 +12,4 @@ const firebaseConfig = {
   measurementId: "G-B7HCHPFNGZ"
 };
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
